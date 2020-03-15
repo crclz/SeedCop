12ad035d0c98e609e68fa56b0f5c6caab7539c33
crclz <crucialize@foxmail.com>
2020-03-15 20:57:58 +0800

    添加项目文件。

---
+++ SeedCop/SeedCop/EmptyCtorAnalyzer.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace SeedCop
+{
+	[DiagnosticAnalyzer(LanguageNames.CSharp)]
+	public class EmptyCtorAnalyzer : DiagnosticAnalyzer
+	{
+		public const string DiagnosticId = "NoEmptyCtor";
+
+		// You can change these strings in the Resources.resx file. If you do not want your analyzer to be localize-able, you can use regular strings for Title and MessageFormat.
+		// See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/Localizing%20Analyzers.md for more on localization
+		private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.CtorAnalyzerTitle), Resources.ResourceManager, typeof(Resources));
+		private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.CtorAnalyzerMessageFormat), Resources.ResourceManager, typeof(Resources));
+		private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.CtorAnalyzerDescription), Resources.ResourceManager, typeof(Resources));
+		private const string Category = "Usage";
+
+		private static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
+
+		public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
+
+		public override void Initialize(AnalysisContext context)
+		{
+			// TODO: Consider registering other actions that act on syntax instead of or in addition to symbols
+			// See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/Analyzer%20Actions%20Semantics.md for more information
+			context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.Analyze | GeneratedCodeAnalysisFlags.None);
+			context.EnableConcurrentExecution();
+			context.RegisterSyntaxNodeAction(AnalyzeNode, SyntaxKind.ClassDeclaration);
+		}
+
+		private void AnalyzeNode(SyntaxNodeAnalysisContext context)
+		{
+			var classDeclaration = (ClassDeclarationSyntax)context.Node;
+
+			var baseListString = classDeclaration.BaseList.ToFullString();
+
+			if (baseListString.Contains("RootEntity") || baseListString.Contains("ValueObject"))
+			{
+				// Then check if there is empty ctor
+
+				var ctorDeclareations = classDeclaration.Members
+					.Where(p => p.Kind() == SyntaxKind.ConstructorDeclaration)
+					.Select(p => (ConstructorDeclarationSyntax)p);
+
+				var containsEmptyCtor = ctorDeclareations.Any(p => !p.ParameterList.Parameters.Any());
+
+				Console.WriteLine(classDeclaration.Identifier.Span.Length);
+
+				if (!containsEmptyCtor)
+					context.ReportDiagnostic(Diagnostic.Create(Rule, classDeclaration.Identifier.GetLocation()));
+			}
+		}
+	}
+}
